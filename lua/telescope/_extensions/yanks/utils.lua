e0fabf3d26d2fc4a5bd2334400c001d282e854dc
Gaël Philippe <gael@weardevx.com>
2026-04-18 14:57:25 +0200

    fix(): avoid deprecated vim.tbl_flatten

--- lua/telescope/_extensions/yanks/utils.lua
+++ lua/telescope/_extensions/yanks/utils.lua
@@ -118,7 +118,7 @@ function Utils.nvim_create_augroups(definitions)
         api.nvim_command('augroup ' .. group_name)
         api.nvim_command('autocmd!')
         for _, def in ipairs(definition) do
-            local command = table.concat(vim.tbl_flatten {'autocmd', def}, ' ')
+            local command = table.concat(vim.list_extend({ 'autocmd' }, def), ' ')
             api.nvim_command(command)
         end
         api.nvim_command('augroup END')
